7c5c070773ed87b64da7735720e47ebd5262dab9
Himanish kochhar <himanish.kochhar@gmail.com>
2024-01-14 23:56:25 +0530

    Use context hook using the child and grand child function

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,26 @@
-import { Counter } from "./Counter"
+import { useEffect, useState, createContext } from "react"
+import {Child} from  './Child'
+
+export const themecontext = createContext()
 function App() {
+  const [isDarkMode, SetIsDarkmode] = useState(false)
+
+  useEffect(() => {
+     document.body.style.backgroundColor = isDarkMode ? "#333" : "#fff"
+     document.body.style.color = isDarkMode ? "#fff" : "#333"
+    
+  },[isDarkMode])
+
+  function toggleTheme(){
+     SetIsDarkmode(d => !d)
+  }
 
   return (
-    <Counter />
+    <themecontext.Provider value={{isDarkMode,toggleTheme}}>
+    <Child />
+    <p> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusantium ducimus, ut mollitia quam cumque fugit.</p>
+    </themecontext.Provider>
+
     )
 }
 
---
+++ src/Child.jsx
@@ -0,0 +1,6 @@
+import { useReducer, useState } from "react";
+import {GrandChild} from './GrandChild'
+
+export function Child(){
+return <GrandChild />
+}
