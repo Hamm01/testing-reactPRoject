118bae05d25ee46d2eb54b8541dde4397c48a3ab
Himanish kochhar <himanish.kochhar@gmail.com>
2024-01-17 18:40:57 +0530

    Use Reducer intoduced and changes in add,  toggle, delete functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+import { useEffect, useReducer, useState } from "react"
 import "./styles.css"
 import { TodoItem } from "./TodoItem"
 // Instructions
@@ -8,46 +8,64 @@ import { TodoItem } from "./TodoItem"
 // 3. Add the ability to delete existing todos
 // 4. Add a form that lets you filter todos by their name and hide completed todos
 
- const LOCAL_STORAGE_KEY = "TODOS_KEY"
+const LOCAL_STORAGE_KEY = "TODOS_KEY"
+const ACTIONS ={
+ADD : "ADD",
+UPDATE: "UPDATE",
+TOGGLE: "TOGGLE",
+DELETE: "DELETE"
+}
+function reducer(todos, {type, payload}){
+
+  switch(type){
+    case ACTIONS.ADD :
+      return [
+        ...todos,
+        { name: payload.name, completed: false, id: crypto.randomUUID() },
+      ]
+    case ACTIONS.TOGGLE : 
+      return todos.map((todo) =>{
+      if(todo.id === payload.id){
+        return {...todo, completed: payload.completed}
+      }
+      return todo
+      })
+    case ACTIONS.DELETE : 
+      return todos.filter(todo => todo.id != payload.id)
+    default :
+     throw new Error(`No action found for ${type}.`)
+  }
+}
 
 function App() {
   const [newTodoName, setNewTodoName] = useState("")
-  const [todos, setTodos] = useState(() => {
+  const [todos, dispatch] = useReducer(reducer,[],initialValue => {
   const value = localStorage.getItem(LOCAL_STORAGE_KEY)
-  if(value === null) return []
+  if(value === null) return initialValue
   return JSON.parse(value)
   })
  
   useEffect(() => {
     localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
   },[todos])
-  
+
   function addNewTodo() {
     if (newTodoName === "") return
 
-    setTodos(currentTodos => {
-      return [
-        ...currentTodos,
-        { name: newTodoName, completed: false, id: crypto.randomUUID() },
-      ]
-    })
+    dispatch({type: ACTIONS.ADD, payload: {name: newTodoName}})
+
     setNewTodoName("")
   }
 
   function toggleTodo(todoId, completed) {
-    setTodos(currentTodos => {
-      return currentTodos.map(todo => {
-        if (todo.id === todoId) return { ...todo, completed }
 
-        return todo
-      })
-    })
+    dispatch({type: ACTIONS.TOGGLE, payload:{id: todoId,completed }})
+  
   }
 
   function deleteTodo(todoId) {
-    setTodos(currentTodos => {
-      return currentTodos.filter(todo => todo.id !== todoId)
-    })
+    dispatch({type:ACTIONS.DELETE , payload: { id: todoId}})
+    
   }
 
   return (
