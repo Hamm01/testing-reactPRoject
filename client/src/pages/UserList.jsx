dbc0e6786ad7885d16dabe8fbbb6e0084ec409f4
Himanish kochhar <himanish.kochhar@gmail.com>
2024-01-21 19:06:29 +0530

    Declared the post route with children and added the loader

--- client/src/pages/UserList.jsx
+++ client/src/pages/UserList.jsx
@@ -1,3 +1,74 @@
 export function UserList() {
-  return <h1>Users</h1>
+  return (
+    <h1>
+      Users asjdfj jsdhfjjufsdufin dsjfsdjfh asdf asjdfj jsdhfjjufsdufin
+      dsjfsdjfhasdf asjdfj jsdhfjjufsdufin dsjfsdjfhasdf asjdfj jsdhfjjufsdufin
+      dsjfsdjfhasdf asjdfj jsdhfjjufsdufin dsjfsdjfhasdf asjdfj jsdhfjjufsdufin
+      dsjfsdjfhasdf asjdfj jsdhfjjufsdufin dsjfsdjfhasdf asjdfj jsdhfjjufsdufin
+      dsjfsdjfhasdf asjdfj jsdhfjjufsdufin dsjfsdjfhasdf asjdfj jsdhfjjufsdufin
+      dsjfsdjfhasdf asjdfj jsdhfjjufsdufin dsjfsdjfhasdf asjdfj jsdhfjjufsdufin
+      dsjfsdjfhasdf asjdfj jsdhfjjufsdufin dsjfsdjfhasdf asjdfj jsdhfjjufsdufin
+      dsjfsdjfhasdf asjdfj jsdhfjjufsdufin dsjfsdjfhasdf asjdfj jsdhfjjufsdufin
+      dsjfsdjfhasdf asjdfj jsdhfjjufsdufin dsjfsdjfhasdf asjdfj jsdhfjjufsdufin
+      dsjfsdjfhasdf asjdfj jsdhfjjufsdufin dsjfsdjfhasdf asjdfj jsdhfjjufsdufin
+      dsjfsdjfhasdf asjdfj jsdhfjjufsdufin dsjfsdjfhasdf asjdfj jsdhfjjufsdufin
+      dsjfsdjfhasdf asjdfj jsdhfjjufsdufin dsjfsdjfhasdf asjdfj jsdhfjjufsdufin
+      dsjfsdjfhasdf asjdfj jsdhfjjufsdufin dsjfsdjfhasdf asjdfj jsdhfjjufsdufin
+      dsjfsdjfhasdf asjdfj jsdhfjjufsdufin dsjfsdjfhasdf asjdfj jsdhfjjufsdufin
+      dsjfsdjfhasdf asjdfj jsdhfjjufsdufin dsjfsdjfhasdf asjdfj jsdhfjjufsdufin
+      dsjfsdjfhasdf asjdfj jsdhfjjufsdufin dsjfsdjfhasdf asjdfj jsdhfjjufsdufin
+      dsjfsdjfhasdf asjdfj jsdhfjjufsdufin dsjfsdjfhasdf asjdfj jsdhfjjufsdufin
+      dsjfsdjfhasdf asjdfj jsdhfjjufsdufin dsjfsdjfhasdf asjdfj jsdhfjjufsdufin
+      dsjfsdjfhasdf asjdfj jsdhfjjufsdufin dsjfsdjfhasdf asjdfj jsdhfjjufsdufin
+      dsjfsdjfhasdf asjdfj jsdhfjjufsdufin dsjfsdjfhasdf asjdfj jsdhfjjufsdufin
+      dsjfsdjfhasdf asjdfj jsdhfjjufsdufin dsjfsdjfhasdf asjdfj jsdhfjjufsdufin
+      dsjfsdjfhasdf asjdfj jsdhfjjufsdufin dsjfsdjfhasdf asjdfj jsdhfjjufsdufin
+      dsjfsdjfhasdf asjdfj jsdhfjjufsdufin dsjfsdjfhasdf asjdfj jsdhfjjufsdufin
+      dsjfsdjfhasdf asjdfj jsdhfjjufsdufin dsjfsdjfhasdf asjdfj jsdhfjjufsdufin
+      dsjfsdjfhasdf asjdfj jsdhfjjufsdufin dsjfsdjfhasdf asjdfj jsdhfjjufsdufin
+      dsjfsdjfhasdf asjdfj jsdhfjjufsdufin dsjfsdjfhasdf asjdfj jsdhfjjufsdufin
+      dsjfsdjfhasdf asjdfj jsdhfjjufsdufin dsjfsdjfhasdf asjdfj jsdhfjjufsdufin
+      dsjfsdjfhasdf asjdfj jsdhfjjufsdufin dsjfsdjfhasdf asjdfj jsdhfjjufsdufin
+      dsjfsdjfhasdf asjdfj jsdhfjjufsdufin dsjfsdjfhasdf asjdfj jsdhfjjufsdufin
+      dsjfsdjfhasdf asjdfj jsdhfjjufsdufin dsjfsdjfhasdf asjdfj jsdhfjjufsdufin
+      dsjfsdjfhasdf asjdfj jsdhfjjufsdufin dsjfsdjfhasdf asjdfj jsdhfjjufsdufin
+      dsjfsdjfhasdf asjdfj jsdhfjjufsdufin dsjfsdjfhasdf asjdfj jsdhfjjufsdufin
+      dsjfsdjfhasdf asjdfj jsdhfjjufsdufin dsjfsdjfhasdf asjdfj jsdhfjjufsdufin
+      dsjfsdjfhasdf asjdfj jsdhfjjufsdufin dsjfsdjasjdfj jsdhfjjufsdufin
+      dsjfsdjfh asdf asjdfj jsdhfjjufsdufin dsjfsdjfhasdf asjdfj jsdhfjjufsdufin
+      dsjfsdjfhasdf asjdfj jsdhfjjufsdufin dsjfsdjfhasdf asjdfj jsdhfjjufsdufin
+      dsjfsdjfhasdf asjdfj jsdhfjjufsdufin dsjfsdjfhasdf asjdfj jsdhfjjufsdufin
+      dsjfsdjfhasdf asjdfj jsdhfjjufsdufin dsjfsdjfhasdf asjdfj jsdhfjjufsdufin
+      dsjfsdjfhasdf asjdfj jsdhfjjufsdufin dsjfsdjfhasdf asjdfj jsdhfjjufsdufin
+      dsjfsdjfhasdf asjdfj jsdhfjjufsdufin dsjfsdjfhasdf asjdfj jsdhfjjufsdufin
+      dsjfsdjfhasdf asjdfj jsdhfjjufsdufin dsjfsdjfhasdf asjdfj jsdhfjjufsdufin
+      dsjfsdjfhasdf asjdfj jsdhfjjufsdufin dsjfsdjfhasdf asjdfj jsdhfjjufsdufin
+      dsjfsdjfhasdf asjdfj jsdhfjjufsdufin dsjfsdjfhasdf asjdfj jsdhfjjufsdufin
+      dsjfsdjfhasdf asjdfj jsdhfjjufsdufin dsjfsdjfhasdf asjdfj jsdhfjjufsdufin
+      dsjfsdjfhasdf asjdfj jsdhfjjufsdufin dsjfsdjfhasdf asjdfj jsdhfjjufsdufin
+      dsjfsdjfhasdf asjdfj jsdhfjjufsdufin dsjfsdjfhasdf asjdfj jsdhfjjufsdufin
+      dsjfsdjfhasdf asjdfj jsdhfjjufsdufin dsjfsdjfhasdf asjdfj jsdhfjjufsdufin
+      dsjfsdjfhasdf asjdfj jsdhfjjufsdufin dsjfsdjfhasdf asjdfj jsdhfjjufsdufin
+      dsjfsdjfhasdf asjdfj jsdhfjjufsdufin dsjfsdjfhasdf asjdfj jsdhfjjufsdufin
+      dsjfsdjfhasdf asjdfj jsdhfjjufsdufin dsjfsdjfhasdf asjdfj jsdhfjjufsdufin
+      dsjfsdjfhasdf asjdfj jsdhfjjufsdufin dsjfsdjfhasdf asjdfj jsdhfjjufsdufin
+      dsjfsdjfhasdf asjdfj jsdhfjjufsdufin dsjfsdjfhasdf asjdfj jsdhfjjufsdufin
+      dsjfsdjfhasdf asjdfj jsdhfjjufsdufin dsjfsdjfhasdf asjdfj jsdhfjjufsdufin
+      dsjfsdjfhasdf asjdfj jsdhfjjufsdufin dsjfsdjfhasdf asjdfj jsdhfjjufsdufin
+      dsjfsdjfhasdf asjdfj jsdhfjjufsdufin dsjfsdjfhasdf asjdfj jsdhfjjufsdufin
+      dsjfsdjfhasdf asjdfj jsdhfjjufsdufin dsjfsdjfhasdf asjdfj jsdhfjjufsdufin
+      dsjfsdjfhasdf asjdfj jsdhfjjufsdufin dsjfsdjfhasdf asjdfj jsdhfjjufsdufin
+      dsjfsdjfhasdf asjdfj jsdhfjjufsdufin dsjfsdjfhasdf asjdfj jsdhfjjufsdufin
+      dsjfsdjfhasdf asjdfj jsdhfjjufsdufin dsjfsdjfhasdf asjdfj jsdhfjjufsdufin
+      dsjfsdjfhasdf asjdfj jsdhfjjufsdufin dsjfsdjfhasdf asjdfj jsdhfjjufsdufin
+      dsjfsdjfhasdf asjdfj jsdhfjjufsdufin dsjfsdjfhasdf asjdfj jsdhfjjufsdufin
+      dsjfsdjfhasdf asjdfj jsdhfjjufsdufin dsjfsdjfhasdf asjdfj jsdhfjjufsdufin
+      dsjfsdjfhasdf asjdfj jsdhfjjufsdufin dsjfsdjfhasdf asjdfj jsdhfjjufsdufin
+      dsjfsdjfhasdf asjdfj jsdhfjjufsdufin dsjfsdjfhasdf asjdfj jsdhfjjufsdufin
+      dsjfsdjfhasdf asjdfj jsdhfjjufsdufin dsjfsdjfhasdf asjdfj jsdhfjjufsdufin
+      dsjfsdjfhasdf asjdfj jsdhfjjufsdufin dsjfsdjfhasdf asjdfj jsdhfjjufsdufin
+      dsjfsdjfhasdf asjdfj jsdhfjjufsdufin dsjfsdjfhasdf asjdfj jsdhfjjufsdufin
+      dsjfsdj
+    </h1>
+  )
 }
--- client/src/router.jsx
+++ client/src/router.jsx
@@ -3,14 +3,28 @@ import { PostList } from './pages/PostList'
 import { UserList } from './pages/UserList'
 import { TodoList } from './pages/TodoList'
 import { RootLayout } from './layouts/RootLayout'
-
+import axios from 'axios'
 export const Router = createBrowserRouter([
   {
     path: '/',
     element: <RootLayout />,
     children: [
       { index: true, element: <Navigate to="/posts" /> },
-      { path: 'posts', element: <PostList /> },
+      {
+        path: 'posts',
+        children: [
+          {
+            index: true,
+            element: <PostList />,
+            loader: ({ request: { signal } }) => {
+              return axios
+                .get('http://localhost:3000/posts', { signal })
+                .then(res => res.json())
+            }
+          },
+          { path: ':postId', element: <h1>Single post</h1> }
+        ]
+      },
       { path: 'users', element: <UserList /> },
       { path: 'todos', element: <TodoList /> }
     ]
