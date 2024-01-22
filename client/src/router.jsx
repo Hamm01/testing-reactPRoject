f270cfacab12338622c3adb4e7cdc6e08db02ceb
Himanish kochhar <himanish.kochhar@gmail.com>
2024-01-22 16:07:59 +0530

    ErrorElement added and route * is added for handling 404..

--- client/src/router.jsx
+++ client/src/router.jsx
@@ -6,30 +6,40 @@ import { postRoute } from './pages/post'
 import { userRoute } from './pages/User'
 import { RootLayout } from './layouts/RootLayout'
 
+// 1. path :"*" route added, this will provide any other route error of 404
+// 2 . errorElement added specificly in children of rootLayout because any axios error or
+// error in component will lead to show message Error.. the root layout , nav bar will be loaded and our error will be
+// displayed below
 export const Router = createBrowserRouter([
   {
     path: '/',
     element: <RootLayout />,
     children: [
-      { index: true, element: <Navigate to="/posts" /> },
       {
-        path: 'posts',
+        errorElement: <h1>Error</h1>,
         children: [
+          { index: true, element: <Navigate to="/posts" /> },
           {
-            index: true,
-            ...postListRoute
+            path: 'posts',
+            children: [
+              {
+                index: true,
+                ...postListRoute
+              },
+              { path: ':postId', ...postRoute }
+            ]
           },
-          { path: ':postId', ...postRoute }
+          {
+            path: 'users',
+            children: [
+              { index: true, ...userListRoute },
+              { path: ':userId', ...userRoute }
+            ]
+          },
+          { path: 'todos', ...todoListRoute },
+          { path: '*', element: <h1>Error -404 Page Not found</h1> }
         ]
-      },
-      {
-        path: 'users',
-        children: [
-          { index: true, ...userListRoute },
-          { path: ':userId', ...userRoute }
-        ]
-      },
-      { path: 'todos', ...todoListRoute }
+      }
     ]
   }
 ])
