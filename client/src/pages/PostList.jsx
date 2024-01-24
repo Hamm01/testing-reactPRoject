367a6d72450d80cba15a6a92955d52e53378dbff
Himanish kochhar <himanish.kochhar@gmail.com>
2024-01-24 19:34:18 +0530

    Advanced part -3rd instruction complete, filter form included in postlist page, and action added based on the filter for url query params

--- client/src/pages/PostList.jsx
+++ client/src/pages/PostList.jsx
@@ -1,9 +1,17 @@
 import axios from 'axios'
-import { Link, useLoaderData } from 'react-router-dom'
+import { Form, Link, useLoaderData } from 'react-router-dom'
 import { getPosts } from '../api/posts'
 import PostCard from '../component/postCard'
+import { useEffect, useRef } from 'react'
 function PostList() {
-  const posts = useLoaderData()
+  const {
+    posts,
+    searchParams: { query }
+  } = useLoaderData()
+  const queryRef = useRef()
+  useEffect(() => {
+    queryRef.current.value = query
+  }, [query])
   return (
     <>
       <h1 className="page-title">
@@ -14,6 +22,31 @@ function PostList() {
           </Link>
         </div>
       </h1>
+      <Form action="/posts" className="form mb-4">
+        <div className="form-row">
+          <div className="form-group">
+            <label htmlFor="query">Query</label>
+            <input type="search" name="query" id="query" ref={queryRef} />
+          </div>
+          {/* <div className="form-group">
+            <label for="userId">Author</label
+            ><select type="search" name="userId" id="userId">
+              <option value="">Any</option>
+              <option value="1">Leanne Graham</option>
+              <option value="2">Ervin Howell</option>
+              <option value="3">Clementine Bauch</option>
+              <option value="4">Patricia Lebsack</option>
+              <option value="5">Chelsey Dietrich</option>
+              <option value="6">Mrs. Dennis Schulist</option>
+              <option value="7">Kurtis Weissnat</option>
+              <option value="8">Nicholas Runolfsdottir V</option>
+              <option value="9">Glenna Reichert</option>
+              <option value="10">Clementina DuBuque</option>
+            </select>
+          </div> */}
+          <button className="btn">Filter</button>
+        </div>
+      </Form>
       <div className="card-grid">
         {posts.map(post => {
           return <PostCard key={post.id} {...post} />
@@ -23,8 +56,12 @@ function PostList() {
   )
 }
 
-function loader({ request: { signal } }) {
-  return getPosts({ signal })
+async function loader({ request: { signal, url } }) {
+  const searchParams = new URL(url).searchParams
+  const query = searchParams.get('query')
+  const filterParams = { q: query }
+  const posts = getPosts({ signal, params: filterParams })
+  return { posts: await posts, searchParams: { query } }
 }
 
 export const postListRoute = {
